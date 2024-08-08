4487c5f3a11dff5e73955f1f152ad72dfaefb295
Carlos Segura <carlos.segurap09@gmail.com>
2024-08-08 14:21:02 -0500

    Actualiza .gitignore para incluir archivos json y migraciones

---
+++ Reservas/Migrations/20240804024537_agregarDocCliente.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservas.Migrations
+{
+    /// <inheritdoc />
+    public partial class agregarDocCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NumeroDocumento",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroDocumento",
+                table: "Clientes");
+        }
+    }
+}
---
+++ Reservas/Migrations/20240805024017_AddEstadoToReserva.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstadoToReserva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Reservas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Reservas");
+        }
+    }
+}
